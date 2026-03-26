51f5fcba11f4ec69efd5be2de1385aec25b59f58
wudjitzu24 <przemekwlodarczyk1442@gmail.com>
2026-03-26 15:42:17 +0100

    Implemented ESLint and Prettier

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from Acquisitions!');
-})
+  res.status(200).send('Hello from Acquisitions!');
+});
 
 export default app;
